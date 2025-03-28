8c655484deaadea83e00f501c812d59cefeab113
Frank Nicolaro <98722559+franknicolaro@users.noreply.github.com>
2025-03-28 11:45:28 -0400

    Update admin.py (linting)

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Register your models here.
-
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 2
+
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
@@ -15,11 +14,12 @@ class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'year')
     list_filter = ['year']
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ['name', 'description']
-    
+
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
